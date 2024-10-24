8e2b50d5a12366aeac0ff05cc6d502df64891c17
Rafiabiassyarif <rafiabiassyarif@gmail.com>
2024-10-24 12:18:30 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,24 @@
 public class Nomor1{
     public static void main(String[] args){
+ Scanner scanner = new Scanner(System.in);
 
+            int totalJamLembur = 0;  
+            double honorLembur = 10.000, tambahanLembur = 0, SisaJamLembur = 15000;
+
+            System.out.print("totalJamLembur: ");
+            totalJamLembur = scanner.nextInt();
+            
+            if ( totalJamLembur == 16) {
+                tambahanLembur = 10000+totalJamLembur;
+            } else if ( totalJamLembur > 16 ) {
+                tambahanLembur= 10000+totalJamLembur;
+            } else if (SisaJamLembur == 0) {
+                tambahanLembur = 15000+totalJamLembur;
+            }
+            
+            honorLembur += tambahanLembur;
+            System.out.println("Honor Lembur: " + honorLembur);
+            
+            scanner.close();
     }
 }
