a240e944a1c7a7168cfb91068c3e3c6b615dcead
patricktelnoni <patricktelnoni@users.noreply.github.com>
2024-10-29 14:48:51 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-    public class HitungHonorLembur {
+    public class Nomor1 {
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
